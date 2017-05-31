aaa3ee865d08dea8afa43de810b6bc68d2aed740
sxw031 <jameswangishere@gmail.com>
2017-05-30 21:03:29 -0700

    modify the state to be stored in the Board component instead of in each Square

--- src/index.js
+++ src/index.js
@@ -11,16 +11,18 @@ import './index.css';
 
 // React components can have state by setting this.state in the constructor, which should be considered private to the component.
 
+//a common convention in React apps to use on* names for the handler prop names and handle* for their implementations.
+
 class Square extends React.Component {
 
 	// add a constructor to the class to initialize the state.
 	// in javaScript classes, you need to explicitly call super(); when defining the constructor of a subclass.
-	constructor() {
-		super();
-		this.state = {
-			value: null,
-		};
-	}
+	// constructor() {
+	// 	super();
+	// 	this.state = {
+	// 		value: null,
+	// 	};
+	// }
 
   	render() {
     	return (
@@ -28,18 +30,40 @@ class Square extends React.Component {
     		// whenever this.setState is called, an update to component is scheduled, causing React to merge in the passed state update and
     		// render the component along with this descendants. 
     		// When the component rerenders, this.state.value will be 'X' so you'll see an X in the grid.
-    		<button className="square" onClick={()=> this.setState({value: 'X'})}>
-        		{this.state.value}
-
-      		</button>
+    		<button className="square" onClick={() => this.props.onClick()}>
+        		{this.props.value}
+      	</button>
     	);
   	}
 }
 
 class Board extends React.Component {
+  // the best solution here is to store this state in the Board component instead of in each Square
+  // and the Board component can tell each Square what to display, like how we made each square display its index earlier.
+
+  // add a constructor to the Board and set its initial state to contain an array with 9 nulls, corresponding to the 9 squares.
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  //define handleClick function
+  handleClick(i) {
+    const squares = this.state. squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-  	//pass a value prop to the Square
-    return <Square value={i} />;
+  	//pass a value prop to the Square, and change what happens when a square is clicked.
+    return (
+      <Square 
+        value={this.state.squares[i]} 
+        onClick={() => this.handleClick(i)} 
+      />
+    );
   }
 
   render() {
